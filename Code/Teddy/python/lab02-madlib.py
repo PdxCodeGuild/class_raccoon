75d5ad4ea99af27fe6f4e586210984f60652eb23
Teddy <tachan072@hotmail.com>
2019-12-09 14:56:57 -0800

    lab 2

---
+++ Code/Teddy/python/lab02-madlib.py
@@ -0,0 +1,12 @@
+animal = input("What is your favorite animal?:...")
+veggi = input("Vegetable:...")
+hungry = input("The felling when you want to eat:...")
+size = input("Your shirt size:...")
+
+print("Let's play a madlibs game")
+print(f'''
+\nToday I went to the zoo.
+\nI saw a {animal} jumping up and down in its tree.
+\nI got some {veggi} and passed them through the cage above my head.
+\nFeeding that animal made me {hungry}.
+\nI went to get a {size} scoop of ice cream.\n''')

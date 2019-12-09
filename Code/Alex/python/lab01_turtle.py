3009d45b4d0e3263393b7a506d6b0eed62704c35
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2019-12-09 14:06:01 -0800

    lab1

---
+++ Code/Alex/python/lab01_turtle.py
@@ -0,0 +1,93 @@
+from turtle import * #turtle is in python. the * means import everything
+# from turtle import color, forward, left, right etc.. (other way to import)
+
+color("green") #color choice
+
+
+forward(100) #units
+left(90) #degrees
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(25)
+'''
+#left eye
+penup()
+left(90)
+forward(70)
+pendown()
+forward(5)
+left(90)
+forward(5)
+left(90)
+forward(5)
+left(90)
+forward(5)
+
+#right eye
+penup()
+forward(25)
+left(90)
+forward(15)
+pendown()
+forward(7)
+right(90)
+forward(7)
+right(90)
+forward(7)
+right(90)
+forward(7)
+
+#mouth
+right(180)
+'''
+
+
+
+forward(65)
+right(45)
+forward(50)
+left(45)
+forward(50)
+right(90)
+forward(40)
+left(130)
+forward(70)
+left(90)
+forward(50) #spot to add hands later
+right(180)
+forward(50)
+right(90)
+forward(70)
+left(50)
+forward(150)
+right(90)
+forward(25)
+left(135)
+forward(70)
+left(180)
+forward(70)
+left(90)
+forward(70)
+left(180)
+forward(70)
+left(135)
+forward(25)
+right(90)
+forward(150)
+left(130)
+forward(70)
+right(90)
+forward(40) #hands
+right(180)
+forward(40)
+left(90)
+forward(70)
+left(50)
+forward(50)
+
+
+done() #if you dont type this, the window will close after it runs

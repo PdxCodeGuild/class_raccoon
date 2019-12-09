71c1dc874e3a981c89f9e0840109676efadab90b
Johnny Phompadith <johnny.phompadith@gmail.com>
2019-12-09 15:59:45 -0800

    update lab02

--- Code/Johnny/python/lab02_madlib.py
+++ Code/Johnny/python/lab02_madlib.py
@@ -6,6 +6,8 @@ sound_var = input("An annoying sound: ")
 animal1 = input("What's your favorite animal: ")
 animal2 = input("Farm animal: ")
 
-output = f"Just waking up in the morning gotta thank {deity_var}, I dont know but today seems kinda odd. No {sound_var} from the {animal1}, no smog and momma cooked the breakfast with no {animal2}."
 
-print(output)
+print(f"Just waking up in the morning gotta thank {deity_var}, ")
+print("I dont know but today seems kinda odd.")
+print(f"No {sound_var} from the {animal1},")
+print(f"No smog and momma cooked the breakfast with no {animal2}. ")

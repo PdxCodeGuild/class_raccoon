c521e32df2baef207ab4220aa7126d81d314bbdb
Tim Galloway <tim@timgalloway.net>
2019-12-10 14:36:41 -0800

    update for lab 6

--- Code/Tim/python/lab06_password_gen.py
+++ Code/Tim/python/lab06_password_gen.py
@@ -8,9 +8,12 @@ characters = int(input("How many characters long would you like your password? "
 password = ""
 
 while len(password) < characters:
-    library = (string.ascii_letters + string.digits + string.punctuation)
+    #original library of characters
+    # library = (string.ascii_letters + string.digits + string.punctuation)
 
+    #library of characters with most common special characters
+    library = (string.ascii_letters + string.digits + "!" + "@" + "#" + "$" + "&" + ".")
     password = password + random.choice(library)
 
 os.system('cls')
-print(f"\n\nYou\'re random password is \n\n\n\n{password}\n\n\n")
+print(f"\n\nYou\'re random password is: \n\n\n\n{password}\n\n\n\nDon\'t lose it!\n\n\n")

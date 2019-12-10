2696b5a2fe7235d8040e63df0c5d1cdd08dfbcef
bgonzalez262 <bgonzo262@gmail.com>
2019-12-10 12:23:48 -0800

    day 2 updated

---
+++ Code/Brandon/python/lab02_madlibs2.py
@@ -0,0 +1,9 @@
+#Randomized madlibs game
+
+import random
+
+print("Welcome to the randomized version of the game")
+
+a,b,c = input("Please enter 3 adjectives, separated by commas.\n")
+x,y = input("Please enter 2 nouns, separated by commas.\n")
+u,t = input("Please enter 2 verb, separated by commas.\n")

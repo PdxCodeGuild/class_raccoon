6d589204cb28156166e29c44b708657df4f0e415
Teddy <tachan072@hotmail.com>
2019-12-10 11:28:47 -0800

    lab03-grading.py

--- Code/Teddy/python/lab03-grading.py
+++ Code/Teddy/python/lab03-grading.py
@@ -3,23 +3,23 @@
 user_input = float(input('What is your score (0 - 100)?'))
 
 # 90-100: A
-if user_input in range(90, 101):
+if user_input >=90 and user_input <= 100:
     print('Your grade is A')
 
 # 80-89: B
-elif user_input in range (80, 90):
+elif user_input >= 80 and user_input < 90:
     print('Your grade is B')
 
 # 70-79: C
-elif user_input in range (70, 80):
+elif user_input >= 70 and user_input < 80:
     print('Your grade is C')
 
 # 60-69: D
-elif user_input in range (60, 70):
+elif user_input >= 60 and user_input < 70:
     print('Your grade is D')
 
 # 0-59: F
-elif user_input in range (0, 60):
+elif user_input >= 0 and user_input < 60:
     print('Your grade is F')
 
 else:

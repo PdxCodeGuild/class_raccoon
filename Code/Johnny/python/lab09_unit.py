369bb3c2369119bf395cd018493b2b91d9c4dc21
Johnny Phompadith <johnny.phompadith@gmail.com>
2019-12-12 11:01:41 -0800

    lab09

---
+++ Code/Johnny/python/lab09_unit.py
@@ -0,0 +1,18 @@
+# lab09_unit.py
+
+# dictionary
+units_dict = {"ft": 0.3048, "mi": 1609.34, "m": 1, "km": 1000,}
+
+
+# save user input
+distance = int(input("What is the distance to meters? "))
+units = input("Is this 'ft', 'mi', 'm', or 'km': ")
+
+# calculate
+if units not in units_dict.keys():
+    print("Invalid input.")
+else:
+    result = distance * units_dict[units]
+    print(f"{distance}{units} is {result}m")
+units_dict["yd"] = 0.9144
+units_dict["in"] = 0.0254

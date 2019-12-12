bba81c66e673b9ad15b5ea66c786c481b83e787b
bgonzalez262 <bgonzo262@gmail.com>
2019-12-12 11:11:21 -0800

    lab_09

--- Code/Brandon/python/lab09_unit.py
+++ Code/Brandon/python/lab09_unit.py
@@ -4,25 +4,26 @@
 unit_dict = { "ft": .3048, "mi": 1609.34, "m": 1, "km": 1000}
 
 def conversion_1(x,measurement):
-    global user_1
+   
     if x == "feet":
-        user_1 = measurement * unit_dict["ft"]
-        return(f"That is {user_1} meters. Below is your requested conversion.")
+        user_1 = measurement // unit_dict["ft"]
+        return float(user_1)
     elif x == "mile":
         user_1 = measurement * unit_dict["mi"]
-        return(f"That is {user_1} meters. Below is your requested conversion.")
+        return float(user_1)
     elif x == "meter":
         user_1 = measurement * unit_dict["m"]
-        return(f"That is {user_1} meters. Below is your requested conversion.")
+        return float(user_1)
     elif x == "kilometer":
         user_1= measurement * unit_dict["km"]
-        return(f"That is {user_1} meters. Below is your requested conversion.")
+        return float(user_1)
 
-def conversion2(y):
+def conversion2(y, user_1):
+    print(user_1)
     
     if y == "meter":
-        # user_2 = user_1 * unit_dict["m"]
-        print(f"{user_1} meters")
+        user_2 = user_1 * float(unit_dict["m"])
+        print(f"{user_2} meters")
     elif y == "feet":
         user_2 = user_1 // unit_dict["ft"]
         print(f"{user_2} feet")
@@ -38,12 +39,12 @@ def conversion2(y):
 #Use info collected to run both functions. conv 1 is the meter conversion, conversion 2 is the output
 while True:
     print("Welcome to the unit converter.")
-    type_1 = input("What type of unit is your measurement? Please enter correct spelling of unit.\n:").lower
+    type_1 = input("What type of unit is your measurement? Please enter correct spelling of unit.\n:").lower()
     measurement = input("Please enter the amount to be converted\n:")
     type_2 = input("What would you like to convert it to?\n:")
     measurement = float(measurement)
-    conversion_1(type_1,measurement)
-    conversion2(type_2)
+    meter_calc = conversion_1(type_1, measurement)
+    conversion2(type_2, meter_calc)
 
     convert_again = input("Would you like to convert again?\n:")
 

8cea5c8fcf3ab180d0839bdfb9b8e502b147d5af
Johnny Phompadith <johnny.phompadith@gmail.com>
2019-12-11 16:12:44 -0800

    lab 07 and lab08

--- Code/Johnny/python/lab06_password.py
+++ Code/Johnny/python/lab06_password.py
@@ -1,11 +1,8 @@
 # lab06_password.py
 """
 Let's generate a password of length n using a while loop and random.choice, this will be a string of random characters.
-
 Hint: random.choice can be used to pick a character out of a string, as well as an element out of a list.
-
 Concepts Covered
-
 input, print
 looping
 random.choice

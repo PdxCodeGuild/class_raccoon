e3375ff4f7acf41757d541114e2ea3f6bfc13761
Johnny Phompadith <johnny.phompadith@gmail.com>
2019-12-13 11:16:24 -0800

    update

--- Code/Johnny/python/lab11_guess_number.py
+++ Code/Johnny/python/lab11_guess_number.py
@@ -3,15 +3,18 @@ import random
 counter = 0
 computer_var = random.randint(1, 10)
 while counter < 10:
+    counter = counter + 1
     player_var = int(input("Guess a number between 1 - 10? "))
     print(f"Player guesses {player_var}.")
-    print(f"Computer's number is {computer_var}.")
     i = player_var
     j = computer_var
     k = player_var - computer_var
-    print(f"You are {k} away from being correct. ")
-    counter = counter + 1
-    if i == j:
+    if i < j:
+        print("Too low.")
+    elif i > j:
+        print("Too high.")
+    elif i == j:
+        print(f"Computer's number is {computer_var}.")
         print(f"You won! {counter} tries. ")
         break
 else:

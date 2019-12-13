052a36f33d442b838d90239ff9b2d5ae5bc33816
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2019-12-13 13:52:17 -0800

    labs

--- Code/Alex/python/lab11_guess_the_number_v3.py
+++ Code/Alex/python/lab11_guess_the_number_v3.py
@@ -14,17 +14,16 @@ Version 3
 
 Tell the user whether their guess is above ('too high!') or below ('too low!') the target value.
 '''
-#todo
 
 
 import random
 
-print("\n\n\n\n\n\n\n\nLet's play 'Guess the Number'. The computer will guess a random int between 1 and 10. You can then try to guess the number, and the program will tell you whether you are right or wrong.\n\n")
+print("\n\n\n\n\n\n\n\nGuess a number between 1 & 10\n\n")
 
 computer_choice = random.randint(1,10)#might need to be 11
 
 
-user_guess = input("Guess the number or type 'done' to cancel: ")
+user_guess = input("or type 'done' to cancel: ")
 
 counter = 0
 
@@ -32,15 +31,26 @@ while user_guess != "done":
 
     counter += 1
 
+    if int(user_guess) > computer_choice:
+        print("\nToo high!")
+    if int(user_guess) < computer_choice:
+        print("\nToo low.")
+
     if int(user_guess) != computer_choice:
         print("\ntry again!\n\n")
 
     if int(user_guess) == computer_choice:
         print("\nYou guessed my number! Good job\n\n")
-        print(f"It took you {counter} tries to guess my number.\n\n")
-        if counter <= 3:
+        print(f"How many tries it took you to guess my number: {counter}\n\n")
+        if counter == 4:
             print("Not bad\n\n\n")
-        if counter > 3:
+        if counter == 3:
+            print("Not bad\n\n\n")
+        if counter == 2:
+            print("Not bad\n\n\n")
+        if counter == 1:
+            print("Perfect!\n\n\n")
+        if counter > 4:
             print("I think you can do better..\n\n\n")
 
-    user_guess = input("Guess the number or type 'done' to cancel: ")
+    user_guess = input("Play again by guessing a number or type 'done' to quit\n\n")

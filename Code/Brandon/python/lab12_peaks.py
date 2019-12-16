ec5474799b87f60264aa4105cfa34b498e6728f2
bgonzalez262 <bgonzo262@gmail.com>
2019-12-16 11:28:26 -0800

    update Brandon

--- Code/Brandon/python/lab12_peaks.py
+++ Code/Brandon/python/lab12_peaks.py
@@ -3,8 +3,18 @@
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 def data_values(data):
-    for value in data:
-        print("x"*value)
+    exes = ""
+    count = max(data)
+    while count > 0:
+        for i in range(len(data)):
+            if data[i] >= count:
+                exes += "x"
+
+            else:
+                exes += " "
+        count -= 1
+        exes += "\n"
+    return(exes)
     
 def peaks(data):
     pk_list_peaks = []
@@ -14,7 +24,7 @@ def peaks(data):
             pk_list_peaks.append(data[i])
         elif data[i] < data[i-1] and data[i] < data[i+1]:
             pk_list_valleys.append(data[i])
-    return(pk_list_valleys,pk_list_peaks)
+    return(f"valleys = {pk_list_valleys} peaks =  {pk_list_peaks}")
 
-data_values(data)
+print(data_values(data))
 print(peaks(data))

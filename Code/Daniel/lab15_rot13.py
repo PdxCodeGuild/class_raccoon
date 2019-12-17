18796822194c8316b22b9e9eed013b22ed2a5ca5
Dadams4786 <58703714+Dadams4786@users.noreply.github.com>
2019-12-17 12:27:37 -0800

    added numbers and punctuation to rot13 lab

--- Code/Daniel/lab15_rot13.py
+++ Code/Daniel/lab15_rot13.py
@@ -3,13 +3,13 @@ import string
 user_phrase = input("What would you like to encrypt: ").lower()
 user_encrypt = int(input("What would you like to encrypt it by: "))
 
-alphabet = string.ascii_lowercase
+alphabet = string.ascii_lowercase + string.digits + string.punctuation + " "
 #encrypts user input by adding a user defined number to the index of every letter
 def encrypt(user_phrase, user_encrypt):
     encrypted_phrase = ""
     for i in range(len(user_phrase)):
         x = alphabet.find(user_phrase[i]) + user_encrypt
-        encrypted_digit = alphabet[x%26]
+        encrypted_digit = alphabet[x%(len(alphabet))]
         encrypted_phrase += encrypted_digit
     return encrypted_phrase
 mid_encrypt = encrypt(user_phrase, user_encrypt)
@@ -18,7 +18,7 @@ def decrypt(mid_encrypt, user_encrypt):
     decrypted_phrase = ""
     for i in range(len(mid_encrypt)):
         x = alphabet.find(mid_encrypt[i]) - user_encrypt
-        decrypted_digit = alphabet[x%26]
+        decrypted_digit = alphabet[x%(len(alphabet))]
         decrypted_phrase += decrypted_digit
     return decrypted_phrase
 decrypted_phrase = decrypt(mid_encrypt, user_encrypt)

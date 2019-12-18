d88d001aa48e2743ece11d555fe980440ac79580
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2019-12-18 11:50:06 -0800

    labs

--- Code/Alex/python/lab17_number_to_phrase_v2.py
+++ Code/Alex/python/lab17_number_to_phrase_v2.py
@@ -6,7 +6,7 @@ Version 2
 Handle numbers from 100-999.
 '''
 #user enters the number. this time it can be up to 999.
-number = int(input("Enter a number below 1000: "))
+number = int(input("\n\n\n\n\n\n\n\n\n\nEnter a number below 1000: "))
 
 ones_digit = number % 10#same as before
 tens_digit = number % 100 // 10 #this changed since we now may need the remainder from the hundreds digit to calculate the tens digit.
@@ -59,7 +59,8 @@ hundreds_digit_dict = {
 
 #im using a function to define our two variables being used.
 def num_to_word(hundreds_digit, tens_digit):
-
+    if number == 0:
+        return "\"zero\""
     word = ''
 
     if hundreds_digit >= 1:
@@ -73,11 +74,11 @@ def num_to_word(hundreds_digit, tens_digit):
 
     if hundreds_digit == 0:
         if tens_digit == 0:
-            word += f"{number} is spelled \"{ones_digit_dict[ones_digit]}\""
+            word += f"\"{ones_digit_dict[ones_digit]}\""
         elif tens_digit == 1:
-            word += f"{number} is spelled \"{teens_dict[ones_digit]}\""
+            word += f"\"{teens_dict[ones_digit]}\""
         elif tens_digit > 1:
-            word += f"{number} is spelled \"{tens_digit_dict[tens_digit]}-{ones_digit_dict[ones_digit]}\""
+            word += f"\"{tens_digit_dict[tens_digit]}-{ones_digit_dict[ones_digit]}\""
     return word
 
 #now we are assigning the function to a variable so that we can call it later.

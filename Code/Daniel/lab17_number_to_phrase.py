73166411d76dc1b19856ed6d1f584ce96d3e8ed9
Dadams4786 <58703714+Dadams4786@users.noreply.github.com>
2019-12-17 16:25:30 -0800

    added 100s to numbers to phrase lab

--- Code/Daniel/lab17_number_to_phrase.py
+++ Code/Daniel/lab17_number_to_phrase.py
@@ -1,10 +1,13 @@
 #turn numbers into their word counterparts
 
 single_digits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-teens_tigit = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+teens_digit = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 double_digits = ["ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-user_input = (int(input("Pick a number from 0-99: ")))
+triple_digit = ["one hundred and", "two hundred and", "three hundred and", "four hundred and", "five hundred and", "six hundred and", "seven hundred and", "eight hundred and", "nine hundred and"]
+user_input = (int(input("Pick a number from 0-999: ")))
 
+hundredsdigit = user_input // 100
+user_input = user_input % 100
 tensdigit = user_input // 10
 onesdigit = user_input % 10
 
@@ -15,8 +18,8 @@ elif tensdigit == 0:
 elif tensdigit == 1 and onesdigit == 0:
     print("ten")
 elif tensdigit == 1:
-    print(teens_tigit[onesdigit-1])
+    print(teens_digit[onesdigit-1])
+elif hundredsdigit >= 1:
+    print(triple_digit[hundredsdigit-1] + " " + double_digits[tensdigit-1] + " " + single_digits[onesdigit])
 elif tensdigit > 1:
     print(double_digits[tensdigit-1] + " " + single_digits[onesdigit])
-
-    

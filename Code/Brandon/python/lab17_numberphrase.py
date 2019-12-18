d5df2dea7c5e7e502baf1edd012052ef73b4ba99
bgonzalez262 <bgonzo262@gmail.com>
2019-12-18 11:27:28 -0800

    lab17

--- Code/Brandon/python/lab17_numberphrase.py
+++ Code/Brandon/python/lab17_numberphrase.py
@@ -1,23 +1,46 @@
 #lab 17 number to phrase
 
-ones_digit = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six",7:"seven", 8:"eight", 9:"nine"}
-tens_digit = {2:"twenty", 3:"thirty", 4:"forty", 5:"fifty", 6:"sixty", 7:"seventy", 8:"eighty", 9:"ninety"}
+ones_digit = {0: "", 1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six",7:"seven", 8:"eight", 9:"nine"}
+tens_digit = {0:"",1: "ten", 2:"twenty", 3:"thirty", 4:"forty", 5:"fifty", 6:"sixty", 7:"seventy", 8:"eighty", 9:"ninety"}
 
+
+
+#Fuction for calculating hundreds if the value is 3 items in list.
+def num_hundreds(user_input):
+    new_phrase = ""
+    hundreds = int(user_input[0])
+    tens = int(user_input[1])
+    ones = int(user_input[2])
+    new_phrase += ones_digit[hundreds] + " "+ "hundred" +" " + tens_digit[tens] +" "+  ones_digit[ones]
+    return(new_phrase)
+
+
+#Function for calculating tens and ones if the value is 2.
 def num_phrase(user_input):
     new_phrase = ""
     tens = int(user_input[0])
     ones = int(user_input[1])
-    if tens >= 2:
-        if ones == 0:
-            new_phrase += tens_digit[tens]
-            return(new_phrase)
-        else:
-            new_phrase += tens_digit[tens] +" " +ones_digit[ones]
-            return(new_phrase)
+    if tens == 0 and ones == 0:
+        return("Zero")
+    elif tens == 0:
+        new_phrase += ones_digit[ones]
+        return(new_phrase)        
+    elif ones == 0:
+        new_phrase += tens_digit[tens]
+        return(new_phrase)
+    elif tens == 1 and ones == 0:
+        return("ten")
+    else:
+        new_phrase += tens_digit[tens] +" " +ones_digit[ones]
+        return(new_phrase)
+
+#Function for teens if the tens value is 1
+def num_teen(user_input):
+    tens = int(user_input[0])
+    ones = int(user_input[1])
+
     
-    elif tens == 1 and ones == 1:
-        return("eleven")
-    elif tens == 1 and ones == 2:
+    if tens == 1 and ones == 2:
         return("twelve")
     elif tens == 1 and ones == 3:
         return("thirteen")
@@ -34,7 +57,31 @@ def num_phrase(user_input):
     elif tens == 1 and ones == 9:
         return("nineteen")
 
-        
+def check_num(value,user_input):
+    tens = int(user_input[0])
+    ones = int(user_input[1])
+    
 
+    # if value == 2 and tens + ones <= 9:
+    #     print(num_phrase(user_input))
+    if value == 2:
+        if tens == 1:
+            print(num_teen(user_input))
+        elif tens >= 2:
+            print(num_phrase(user_input))
+        elif value == 2: 
+            print(num_phrase(user_input))
+    elif value == 3:
+        hundreds = int(user_input[2])
+        print(num_hundreds(user_input))
+    
+
+
+ 
+
+        
 user_input = list(input("Enter a number in the range of 0-99. ie 09,11,46\n:"))
-print(num_phrase(user_input))
+value = int(len(user_input))
+print(value)
+check_num(value,user_input)
+

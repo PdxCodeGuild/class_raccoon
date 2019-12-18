7a5954641d00849e9b6d85c6b77668fe629a77cb
Dylan <mrdlesniak@gmail.com>
2019-12-17 16:02:55 -0800

    fixed 'zero' glitch.

--- Code/dylan_lesniak/python/lab17_number_to_phrase.py
+++ Code/dylan_lesniak/python/lab17_number_to_phrase.py
@@ -6,6 +6,8 @@ Purpose: Take input and convert int to phrase. Assume 0-999
 import helper
 
 def convert(num):
+    if num == "0":
+        return "zero"
     singles_dict = {"0":"","1":"one","2":"two","3":"three","4":"four","5":"five","6":"six","7":"seven","8":"eight","9":"nine"}
     num_list = list(num)
     num_str = ""

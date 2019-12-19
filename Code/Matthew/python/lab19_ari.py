7e102831a5a80ce5fa8a34c92835151be503ca85
flux2341 <flux2341@gmail.com>
2019-12-19 12:13:19 -0800

    lab 18, got random book text

---
+++ Code/Matthew/python/lab19_ari.py
@@ -0,0 +1,29 @@
+
+
+
+
+import requests
+import re
+import random
+
+def get_random_book_code():
+    response = requests.get('https://www.gutenberg.org/ebooks/search/?sort_order=random')
+    text = response.text
+    # print(text)
+    codes = re.findall('/ebooks/(\d+)', text)
+    return random.choice(codes)
+    # return random.choice(urls)
+    
+# https://www.gutenberg.org/files/58714/58714-0.txt
+# http://www.gutenberg.org/cache/epub/5785/pg5785.txt
+def get_random_book_text():
+
+    code = get_random_book_code()
+
+    url = f'https://www.gutenberg.org/files/{code}/{code}-0.txt'
+    response = requests.get(url)
+    if response.status_code == 404:
+        url = f'http://www.gutenberg.org/cache/epub/{code}/pg{code}.txt'
+        response = requests.get(url)
+
+    return response.text

526d2d2be518ac92d1b164f56e976e3742ca4171
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2020-01-02 09:49:24 -0800

    lab21_rain_data_v2

---
+++ Code/Alex/python/labs/lab21_rain_data_v2.py
@@ -0,0 +1,23 @@
+'''
+Lab 21: Rain data
+
+Version 2
+
+Now that you've successfully extracted the data, let's done some statistics.
+
+1. Calculate the mean of the data:
+
+mean
+
+2. Use the mean to calculate the standard deviation, which is a measure of how "spread out" the data is:
+
+standard_deviation
+
+68.2% of the data falls within 1 standard deviation of the mean.
+
+bell_curve
+
+3. Find the day which had the most rain.
+
+4. Find the year which had the most rain on average.
+'''

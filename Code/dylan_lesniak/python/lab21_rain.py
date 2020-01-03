c36d61a85643ce07890db67fa6ca6e90dc79fbcd
Dylan <mrdlesniak@gmail.com>
2020-01-03 11:26:13 -0800

    created float_helper in helper.py. Created ATM class up to version 3.

--- Code/dylan_lesniak/python/lab21_rain.py
+++ Code/dylan_lesniak/python/lab21_rain.py
@@ -10,6 +10,10 @@ import re
 import datetime
 import math
 import matplotlib.pyplot as plt
+import helper 
+from inspect import currentframe, getframeinfo
+import numpy as np
+
 
 def get_rain_urls(): 
     response = requests.get('https://or.water.usgs.gov/non-usgs/bes/')
@@ -41,7 +45,21 @@ def get_page_info(file):
     print(f"The day with the most rain was {day_max[0]} with a total of {day_max[1]} inches.")
     print(f"The year with the most rain was {year_max}.")
     
-    return year_avg
+    print("\nWould you like a graph of all the data (total), monthly average (month), or yearly rainfall(year)? ")
+    graph_type = helper.text_checker(input("> "),["total","month","year"])
+    if graph_type == "total":
+        days_dict = get_days_dict(data_dicts)
+        show_graph(days_dict)
+    elif graph_type == "month":
+        month_dict = get_month_dict(data_dicts)
+        show_graph(month_dict)
+    elif graph_type == "year":
+        pass
+    else:
+        print("ERROR: You should not have gotten here.")
+        frameinfo = getframeinfo(currentframe())
+        print ("line: ",frameinfo.lineno,"exit.")
+    return 
 
 
 def get_data_dicts(data_lines): 
@@ -82,6 +100,8 @@ def get_day_max(data_dicts):
     daymax = daymax["Date"].strftime('%d-%b-%Y'), daymax["Daily Total"]
     return daymax
 
+
+
 #make a dict of dicts. Year and then two values for total and days. 
 def get_year_avg(data_dicts):
     year_values = {}
@@ -99,23 +119,58 @@ def get_year_avg(data_dicts):
         year_avgs[year] = (year_values[year]["total"] / year_values[year]["days"])
     return year_avgs
 
-def get_xy_values(year_avgs):
+def get_days_dict(data_dicts):
+    days_dict = {}
+    for day in data_dicts:
+        date = day["Date"].strftime('%d-%b-%Y')
+        daily_total = day["Daily Total"]
+        days_dict[date] = daily_total
+    return days_dict
+
+def get_month_dict(data_dicts):
+    months_dict = {}
+    for day in data_dicts:
+        month = day["Date"].month
+        daily_total = day["Daily Total"]
+        if month in months_dict:
+            if "rain" in months_dict[month]:
+                months_dict[month]["rain"] += daily_total
+                months_dict[month]["days"] += 1
+            else:
+                months_dict[month]["rain"] = daily_total
+                months_dict[month]["days"] = 1
+        else:
+            months_dict[month] = {"rain": daily_total, "days": 1}
+    months_avg = {}
+    months_list = [month for month in months_dict]
+    months_list.sort()
+    for month in months_list:
+        months_avg[datetime.date(1900,month,1).strftime('%B')] = round(months_dict[month]["rain"] / months_dict[month]["days"], 2)
+    return months_avg
+
+
+def get_xy_values(graph_dictionary):
     x_values = []
     y_values = []
-    for year in year_avgs:
+    for year in graph_dictionary:
         x_values.append(year)
-        y_values.append(round(year_avgs[year],2))
+        y_values.append(round(graph_dictionary[year],2))
     xy_values = [x_values,y_values]
     return xy_values  
+
+def show_graph(dict_to_show): 
+    xy = get_xy_values(dict_to_show)
+    month = np.array(xy[0])
+    y = xy[1]
+    plt.xticks(range(len(y)), month)
+    plt.plot(y)
+    plt.show()
+
 
 if __name__ == "__main__":
     rains = get_rain_urls()
     file = requests.get(rains[3])
-    year_avgs = get_page_info(file)
-    xy_values = get_xy_values(year_avgs)
-    x_values = xy_values[0]
-    y_values = xy_values[1]
-    plt.plot(x_values, y_values)
-    plt.show()
+    get_page_info(file)
+    
 
 #data starts at array value 11. This is consistent on the forms, thankfully. 

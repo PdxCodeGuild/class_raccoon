23b98e986a4057f6fa9146f8baf8851cc9d79cae
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2020-01-03 14:21:15 -0800

    lab23_computer_science_data_structures

---
+++ Code/Alex/lab23_computer_science_data_structures.py
@@ -0,0 +1,10 @@
+'''
+Lab 23: Computer Science - Data Structures
+
+Using the following Node class, let's implement some data structures.
+
+class Node:
+  def __init__(element, next=None):
+    self.element = element
+    self.next = next
+'''

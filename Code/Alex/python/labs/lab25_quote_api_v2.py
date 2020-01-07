beaf6b0bb397c0d1789699a907d384e0cdcf28d2
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2020-01-07 15:52:13 -0800

    lab25_quote_api_v2

--- Code/Alex/python/labs/lab25_quote_api_v2.py
+++ Code/Alex/python/labs/lab25_quote_api_v2.py
@@ -13,9 +13,45 @@ The Favqs Quote API also supports getting a list of quotes associated with a giv
 <list of quotes>
 > enter 'next page' or 'done': done
 > enter a keyword to search for quotes:
+
+
 This API endpoint requires an API key be included in a request header. You can find documentation of specifying request headers here and documentation on authorization with the Favqs API here under "Authorization".
 
 url = 'https://favqs.com/api/quotes?page=1&filter=nature'
 headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
 response = requests.get(url, headers=headers)
 '''
+
+
+import requests
+import json
+import os
+
+def quotes(keyword, quote):
+    url = 'https://favqs.com/api/qotd'
+    response = requests.get(url)
+    #print(response.text)
+    data = json.loads(response.text)
+    body = data['quote']['body']
+    author = data['quote']['author']
+
+    print(f"\"{body}\"\n-{author}")
+
+
+keyword = input("enter a keyword to search for quotes: ").lower()
+page = 1
+
+while True:
+    os.system("clear")
+    
+    new_url = f'https://favqs.com/api/quotes?page={page}&filter={keyword}'
+    response = requests.get(new_url)
+    #data = json.loads(response.text)
+    print(f"{response}\n\n")
+
+    page_or_quit = input("enter 'next page' or 'done': ").lower()
+    if page_or_quit == "next page":
+        page += 1
+        if page_or_quit == 'done':
+            break
+

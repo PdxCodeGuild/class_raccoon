2990feb0f2123db0a7e5c71836eba313b6a0630c
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2020-01-08 12:35:03 -0800

    lab26_any_api

--- Code/Alex/python/labs/lab26_any_api.py
+++ Code/Alex/python/labs/lab26_any_api.py
@@ -15,3 +15,62 @@ TheCatAPI - data about cats
 PokeAPI - data about pokemon
 Brickset API - data about legos responds with XML
 '''
+
+
+import webbrowser
+import requests
+import os
+import random
+
+while True:
+    os.system("clear")
+    user_choice = input("Type \"random\" to display a random gif, \"search\" to search for gifs or \"quit\" to quit. ").lower()
+#random
+    if user_choice == "random":
+        os.system("clear")
+        headers = {"api_key" : "rHH2bcJvdAtFJag6LLxFcAnHHZsfHBOm"}
+        url = 'http://api.giphy.com/v1/gifs/random'
+        response = requests.get(url, params=headers)
+        data = response.json()
+        gif = data["data"]["url"]
+        webbrowser.open_new_tab(gif)
+        print()
+        print()
+        random_again = input("Display another random gif? ").lower()
+        while random_again == "yes" or random_again == "y":
+            os.system("clear")
+            url = 'http://api.giphy.com/v1/gifs/random'
+            response = requests.get(url, params=headers)
+            data = response.json()
+            gif = data["data"]["url"]
+            webbrowser.open_new_tab(gif)
+            print()
+            print()
+            random_again = input("Display another random gif? ").lower()
+        else:
+            continue
+#search
+    elif user_choice == "search":
+        term = input("What kind of gif are you looking for? ")
+        headers = {"api_key" : "rHH2bcJvdAtFJag6LLxFcAnHHZsfHBOm", "q" : term}
+        url = f'http://api.giphy.com/v1/gifs/search'
+        response = requests.get(url, params=headers)
+        data = response.json()
+        search_again = "y"
+        while search_again == "yes" or search_again == "y":
+            os.system("clear")
+            gifs = data["data"]
+            gif = random.choice(gifs)
+            webbrowser.open_new_tab(gif["url"])
+            print()
+            print()
+            search_again = input("Search for another gif? ").lower()
+            continue
+        else:
+            continue
+#quit
+    elif user_choice == "quit":
+        break
+#else
+    else:
+        input("invalid entry")

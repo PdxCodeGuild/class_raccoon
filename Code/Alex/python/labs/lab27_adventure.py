c13caa175cb11332c87ea4bc4e994ae261ce2694
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2020-01-09 11:46:46 -0800

    Organized my multiline instructions comment.

--- Code/Alex/python/labs/lab27_adventure.py
+++ Code/Alex/python/labs/lab27_adventure.py
@@ -5,21 +5,35 @@ Let's build a simple board game that runs on the terminal. We'll represent the b
 
 Possible modifications:
 
-use more succinct commands (l/u/d/r for up/down/left/right or n/s/e/w for north/south/east/west)
-add boundaries to the map, when the player attempts to move beyond the boundary, stop them or move them to the other side
-make what's printed on the screen a part of a much larger map (with the player always shown at the center of the screen)
-loading a text file containing the map, or procedurally generate things
-walls / barriers
-use different unicode characters (you can find lists online)
-ascii art
-colorama for custom colors, or curses for even more control of the terminal
-add 'fog of war' - only show the elements of board immediately around the player (you can then find a torch item, which expands your visibility)
-have enemies move around
-add an inventory system
-add player health, more complex encounters
-add hidden treasure, make the objective to find all the treasure
-add a 'final boss' that you can only face once collecting items
-re-use previous labs (guess the number, rock-paper-scissors)
+- use more succinct commands (l/u/d/r for up/down/left/right or n/s/e/w for north/south/east/west)
+
+- add boundaries to the map, when the player attempts to move beyond the boundary, stop them or move them to the other side
+
+- make what's printed on the screen a part of a much larger map (with the player always shown at the center of the screen)
+
+- loading a text file containing the map, or procedurally generate things
+
+- walls / barriers
+
+- use different unicode characters (you can find lists online)
+
+- ascii art
+
+- colorama for custom colors, or curses for even more control of the terminal
+
+- add 'fog of war' - only show the elements of board immediately around the player (you can then find a torch item, which expands your visibility)
+
+- have enemies move around
+
+- add an inventory system
+
+- add player health, more complex encounters
+
+- add hidden treasure, make the objective to find all the treasure
+
+- add a 'final boss' that you can only face once collecting items
+
+- re-use previous labs (guess the number, rock-paper-scissors)
 '''
 
 

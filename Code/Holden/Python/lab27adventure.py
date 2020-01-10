69413b34b99d3dba91f88635895e9afbb5d31401
flarespeed <hflare@gmail.com>
2020-01-10 10:20:45 -0800

    adventure framework complete, beginning work on display

--- Code/Holden/Python/lab27adventure.py
+++ Code/Holden/Python/lab27adventure.py
@@ -1,59 +1,66 @@
 import random
 import os
+import sys
 
-width = 10  # the width of the board
-height = 10  # the height of the board
+width = 30  # the width of the board
+height = 30  # the height of the board
+player_start = [random.randint(0,width-1),random.randint(0,height-1)]
+entity_list = []
+enemy_dict = {"҉":[10,0,1,1], "§":[80,12,18,4], "ß":[20,6,10,2], "ʘ":[40,30,40,10], "ˢ":[2, 2, 18, 2], "ѱ":[120,2,5,2]}
+player = {"☺":[100, 4, 8, 2]}
 
-# create a board with the given width and height
-# we'll use a list of list to represent the board
-board = []  # start with an empty list
-for i in range(height):  # loop over the rows
-    board.append([])  # append an empty row
-    for j in range(width):  # loop over the columns
-        board[i].append(' ')  # append an empty space to the board
 
-# define the player position
-player_i = 4
-player_j = 4
 
-# add 4 enemies in random locations
-for i in range(4):
-    enemy_i = random.randint(0, height - 1)
-    enemy_j = random.randint(0, width - 1)
-    board[enemy_i][enemy_j] = '§'
+class map:
+    def __init__(self,w,h):
+        self.positions = []
+        self.x = w
+        self.y = h
+        for i in range(h):
+            self.positions.append([])
+            for j in range(w):
+                self.positions[i].append("░")
 
-# loop until the user says 'done' or dies
-while True:
-    # print out the board
-    for i in range(height):
-        for j in range(width):
-            # if we're at the player location, print the player icon
-            if i == player_i and j == player_j:
-                print('☺', end=' ')
-            else:
-                print(board[i][j], end=' ')  # otherwise print the board square
-        print()
+    def pos_check(self, position):
+        return self.positions[position[0]][position[1]]
 
-    command = input('what is your command? ')  # get the command from the user
+    def update(self, type, prev=player_start, current):
+        self.positions[prev[0]][prev[1]] = "░"
+        self.positions[current[0]][current[1]] = type
 
-    if command == 'done':
-        break  # exit the game
-    elif command == 'left':
-        player_j -= 1  # move left
-    elif command == 'right':
-        player_j += 1  # move right
-    elif command == 'up':
-        player_i -= 1  # move up
-    elif command == 'down':
-        player_i += 1  # move down
-
-    # check if the player is on the same space as an enemy
-    if board[player_i][player_j] == '§':
-        print('you\'ve encountered an enemy!')
-        action = input('what will you do? ')
-        if action == 'attack':
-            print('you\'ve slain the enemy')
-            board[player_i][player_j] = ' '  # remove the enemy from the board
+def conflict(curr_ent, collided, collided_pos):
+    if curr_ent.type == collided:
+        return
+    elif collided in enemy_dict or collided == "☺":
+        if curr_ent.type == "☺":
+            player_combat(curr_ent, collided_pos)
         else:
-            print('you hestitated and were slain')
-            break
+            enemy_combat(curr_ent, collided_pos)
+    if collided == "ރ" and current.type == "☺":
+        player_weapon = 1
+        curr_ent.map.update(curr_ent.type, [curr_ent.x, curr_ent.y], collided)
+
+class entity:
+    def __init__(self, type="ß", hp=20, lo=6, hi=10, mov=1, map=standard_map, x, y):
+        self.type = type
+        self.hp = hp
+        self.lo = lo
+        self.hi = hi
+        self.mov = mov
+        self.x = x
+        self.y = y
+        self.map = map
+        map.update(type,[x,y])
+
+    def move(self, x, y):
+        newpos = [(self.x + x)%self.map.x, (self.y + y)%self.map.y]
+        position_data = self.map.pos_check(newpos)
+        if position_data == "░":
+            self.map.update(self.type, [self.x, self.y], newpos)
+        else:
+            conflict(self, position_data, newpos)
+
+def player_combat(player, enemy_pos):
+    pass
+def enemy_combat(attacker, def_pos):
+    pass

d4ebc48ede684be0db790827d129d1347336a3ad
Matthew Cooper <flux2341@gmail.com>
2020-01-24 12:20:22 -0800

    added todo list app

--- 3 Django/solutions/lab01_todo/app.py
+++ 3 Django/solutions/lab01_todo/app.py
@@ -1,6 +1,6 @@
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import json
 
 app = Flask(__name__)
@@ -12,22 +12,42 @@ def read_database():
     return data
 
 def write_database(data):
-    data = json.dumps(data)
+    data = json.dumps(data, indent=2)
     with open('database.json', 'w') as file:
         file.write(data)
 
-@app.route('/'. methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     data = read_database()
-    if request.method == 'POST':
-        text = request.form['text']
-        priority = request.form['priority']
-        todo = {
-            'text': text,
-            'priority': priority
-        }
-        data['todos'].append(todo)
-        write_database(data)
+
+    # words = [('apple', 3), ('banana', 4), ('pineapple', 8)]
+    # priorities = {'1': 'high', '2': 'medium', '3': 'low'}
+    # data['todos'].sort(key=lambda x: x['priority'])
+
     return render_template('index.html', todos=data['todos'])
+
+
+@app.route('/savetodo/', methods=['POST'])
+def savetodo():
+    text = request.form['todo_text']
+    priority = request.form['todo_priority']
+    todo = {
+        'text': text,
+        'priority': priority
+    }
+    data = read_database()
+    data['todos'].append(todo)
+    write_database(data)
+    return redirect('/')
+
+
+@app.route('/deletetodo/', methods=['POST'])
+def deletetodo():
+    todo_index = int(request.form['todo_index'])
+    data = read_database()
+    data['todos'].pop(todo_index)
+    write_database(data)
+    return redirect('/')
+
 
 

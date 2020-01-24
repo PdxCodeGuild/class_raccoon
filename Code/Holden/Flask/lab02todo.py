b0828b3696e6769505d1213ceed1c2afd42e14e8
flarespeed <hflare@gmail.com>
2020-01-24 11:22:24 -0800

    added priority sorting

--- Code/Holden/Flask/lab02todo.py
+++ Code/Holden/Flask/lab02todo.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template, request, redirect
 import json
 app = Flask(__name__)
 
+
+
+
 @app.route('/')
 def todo():
     todo_list = []
     with open(r"lab02\database.json", "r") as file:
         data = json.loads(file.read())
     for item in data["todos"]:
-        todo_list.append(item["text"])
+        todo_list.append(item)
     return render_template('lab02todo.html', todo_list=todo_list)
 
 @app.route('/save/', methods=['POST'])
@@ -17,15 +20,34 @@ def database_save():
     with open(r"lab02\database.json", "r+") as file:
         data = json.loads(file.read())
         new_todo = request.form["new_todo"]
+        new_priority = request.form['new_priority']
         not_in_list = True
-        dataout = {"todos": []}
+        high_p = []
+        med_p = []
+        lo_p = []
+        no_p =[]
         for item in data["todos"]:
             if item["text"] not in request.form:
-                dataout["todos"].append(item)
+                if item['priority'] == 'high':
+                    high_p.append(item)
+                if item['priority'] == 'medium':
+                    med_p.append(item)
+                if item['priority'] == 'low':
+                    lo_p.append(item)
+                if item['priority'] == '':
+                    no_p.append(item)
             if item["text"] == new_todo:
                 not_in_list = False
         if not_in_list and new_todo:
-            dataout["todos"].append({"text": new_todo})
+            if new_priority == 'high':
+                high_p.append({'text': new_todo, 'priority': new_priority})
+            if new_priority == 'medium':
+                med_p.append({'text': new_todo, 'priority': new_priority})
+            if new_priority == 'low':
+                lo_p.append({'text': new_todo, 'priority': new_priority})
+            if new_priority == '':
+                no_p.append({'text': new_todo, 'priority': new_priority})
+        dataout = {"todos": high_p + med_p + lo_p + no_p}
         file.seek(0)
         file.write(json.dumps(dataout, indent=2))
         file.truncate()

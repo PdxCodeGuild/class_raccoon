83e07cec1a45219a60dbcda10c44709a1ff9ba0c
bgonzalez262 <bgonzo262@gmail.com>
2020-02-06 16:10:06 -0800

    completed minus the doc link

--- Code/Brandon/Django/labs/Contacts/views.py
+++ Code/Brandon/Django/labs/Contacts/views.py
@@ -7,13 +7,17 @@ from .models import Contact
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.order_by('last_name')
     context = {'contacts':contacts}
 
     return render(request,'Contacts/index.html',context)
 
-def contact_view(request):
-    pass
+def contact_view(request,contact_id):
+    contact = Contact.objects.get(id=contact_id)
+    context = {'contact': contact}
+
+    return render(request,'Contacts/create.html',context)
+
 
 def new_contact(request):
     first = request.POST['first']
@@ -26,15 +30,28 @@ def new_contact(request):
 
     return redirect('Contacts:index')
 
-def edit_contact(request):
-    pass
-
-def delete_contact(request):
-    cont_id = request.POST['id']
+def edit_contact(request,contact_id):
     contact = Contact.objects.get(id=contact_id)
+    first = request.POST['first']
+    last = request.POST['last']
+    birthday = request.POST['birthday']
+    phone = request.POST['phone']
+
+    contact.first_name = first
+    contact.last_name = last
+    contact.birthday = birthday
+    contact.phone_number = phone
+
+    contact.save()
+    return redirect('Contacts:index')
+
+
+def delete_contact(request, contact_id):
+    contact = Contact.objects.get(id=contact_id)
+    print(contact)
     contact.delete()
     return redirect('Contacts:index')
 
 def documentation(request):
-    return render(request, 'Contacts/create.html')
+    return render(request, 'Contacts/doc.html')
 

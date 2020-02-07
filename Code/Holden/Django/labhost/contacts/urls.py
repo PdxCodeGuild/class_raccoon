c204c7e5482ac4c7963eb51ecc20cfce81cf6b37
flarespeed <hflare@gmail.com>
2020-02-07 13:27:28 -0800

    finished lab 2

--- Code/Holden/Django/labhost/contacts/urls.py
+++ Code/Holden/Django/labhost/contacts/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = 'contacts'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:contact>/', views.contact_detail, name='contacts_list'),
+    path('<int:contact_id>/', views.contact_detail, name='contact_detail'),
     path('new/', views.new_contact, name='new_contact'),
-    path('<int:contact>/edit/', views.edit_contact, name='edit_contact'),
+    path('<int:contact_id>/edit/', views.edit_contact, name='edit_contact'),
     path('new/submit/', views.write_new_contact, name='write_new_contact'),
-    path('<int:contact>/edit/submit/', views.write_edit_contact, name='write_edit_contact'),
+    path('<int:contact_id>/edit/submit/', views.write_edit_contact, name='write_edit_contact'),
+    path('<int:contact_id>/delete/', views.delete_contact, name='delete_contact'),
 ]

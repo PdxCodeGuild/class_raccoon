ab0ba7a9e31555f5b8b9c53599a4d65eb9eadec6
Teddy <teddy.photesri@gmail.com>
2020-02-10 15:30:52 -0800

    update

---
+++ Code/Teddy/Django/blogproj/blogapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class BlogPostType(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    # id = models.IntegerField...
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    body = models.TextField()
+    type = models.ForeignKey(BlogPostType, on_delete=models.PROTECT, related_name='posts')
+    date_published = models.DateTimeField(auto_now_add=True)
+
+    def html_body(self):
+        return self.body.replace('\n', '<br/>')
+
+    # print title and author on post blog
+    def __str__(self):
+        return self.title + ' - ' + self.author

1330ee3d3b07377ebd5dd7cc32cc193db6c0f853
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2020-02-10 15:45:06 -0800

    lab3 django

--- Code/Alex/django/mysite/contacts/urls.py
+++ Code/Alex/django/mysite/contacts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
 from . import views
+
 
 app_name = 'contacts'
 
@@ -10,6 +10,5 @@ urlpatterns = [
     path('new_contact', views.new_contact, name='new_contact'),
     path('edit_contact', views.edit_contact, name='edit_contact'),
     path('delete_contact', views.delete_contact, name='delete_contact'),
-    path('documentation', views.documentation, name='documentation')
-
+    path('documentation', views.documentation, name='documentation'),
 ]
--- Code/Alex/django/mysite/mysite/urls.py
+++ Code/Alex/django/mysite/mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('contacts/', include('contacts.urls')),
+    path('urlshortener/', include('urlshortener.urls')),
 ]

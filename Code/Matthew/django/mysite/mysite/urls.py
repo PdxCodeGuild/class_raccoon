f9f72b8e0f87c46f3cf1046577c171a1e02a3af7
Matthew Cooper <flux2341@gmail.com>
2020-02-11 12:38:00 -0800

    added contacts app

--- Code/Matthew/django/mysite/mysite/urls.py
+++ Code/Matthew/django/mysite/mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('rps/', include('rpsgame.urls')),
+    path('contacts/', include('contacts.urls')),
 ]

5496d834f980afa121634ab52f484c13f43912be
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2020-02-11 16:03:19 -0800

    lab3 django

--- Code/Alex/django/mysite/urlshortener/urls.py
+++ Code/Alex/django/mysite/urlshortener/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'urlshortener'
 urlpatterns = [
-  path('index/', views.index, name='index')
+  path('', views.index, name='index'),
+  path('saveurl/', views.saveurl, name='saveurl'),
+  path('redirect/', views.redirect, name='redirect'),
 ]

d06415250535004e1402a84d9ca13fe1a7399a44
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2020-02-14 13:37:38 -0800

    lab3 django

--- Code/Alex/django/mysite/library/models.py
+++ Code/Alex/django/mysite/library/models.py
@@ -6,13 +6,13 @@ class Author(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.title + ' - ' + self.author
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    date_published = models.DateTimeField()
-    author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    year_published = models.IntegerField()
+    authors = models.ManyToManyField(Author, related_name='books')
     checked_out_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.title + ' - ' + self.author.name
--- Code/Alex/django/mysite/library/views.py
+++ Code/Alex/django/mysite/library/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+
+
 def index(request):
+    authors = Author.objects.order_by('date_published')
     context = {
-        'message': 'Hello World!'
+        'authors': authors
     }
     return render(request, 'library/index.html', context)

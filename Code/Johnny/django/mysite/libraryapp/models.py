e146fea66a97c2a8c00f70a5d50abbbac041cbbc
Johnny Phompadith <johnny.phompadith@gmail.com>
2020-02-17 12:20:55 -0800

    django lib lab

--- Code/Johnny/django/mysite/libraryapp/models.py
+++ Code/Johnny/django/mysite/libraryapp/models.py
@@ -19,3 +19,12 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title + ' (' + str(self.author) + ')'
+
+class BookCheckout(models.Model):
+    checkout_checked_out_by = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    checkout_title = models.ForeignKey(Book, on_delete=models.PROTECT, related_name='checkout')
+    checkout_date = models.DateTimeField(auto_now=False)
+    checkin_date = models.DateTimeField(default=None, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.checkout_title) + ' Out: ' + str(self.checkout_checked_out_by)
--- Code/Johnny/django/mysite/libraryapp/urls.py
+++ Code/Johnny/django/mysite/libraryapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'libraryapp'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('checkout/', views.checkout, name='checkout')
 ]

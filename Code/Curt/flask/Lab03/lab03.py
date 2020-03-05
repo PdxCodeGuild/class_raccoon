6a40be325b7e6712db5f14cd999d5d384af5f8ef
DenJansen <curt.justice1@gmail.com>
2020-03-05 10:37:09 -0800

    Pushing because Brandon is making me paranoid

--- Code/Curt/flask/Lab03/lab03.py
+++ Code/Curt/flask/Lab03/lab03.py
@@ -1,61 +1,71 @@
 #lab03.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
-    query = '''query ($id: Int) {
-        Media (id: $id, type: ANIME) {
-            id
-            title {
-                romaji
-                english
-                native
-            }
-            type
-            format
-            status
-            description
-            startDate {
-                year
-                month
-                day
-            }
-            endDate {
-                year
-                month
-                day
-            }
-            season
-            seasonYear
-            seasonInt
-            episodes
-            duration
-            chapters
-            volumes
-            countryOfOrigin
-            genres
-            isAdult
-            recommendations {
-                nodes {
-                    media{
-                        title{
-                            english
-                        }
+    result_list = []
+    if request.method == 'POST':
+        query = '''query ($title: String, $page: Int, $perPage: Int) {
+            Page (page: $page, perPage: $perPage) {
+                media (search: $title, type: ANIME) {
+                    title {
+                        romaji
+                        english
+                        native
+                    }
+                    coverImage {
+                        large
+                    }
+                    streamingEpisodes {
+                        title
+                        url
+                        site
                     }
                 }
             }
-
-        }
         }'''
+        print(request.form)
+        variables = {
+            'title': request.form['user_input'],
+            'page': 1,
+            'perPage': 10
+        }
 
-    variables = {}
+        url = 'https://graphql.anilist.co'
+        response = requests.post(url, json={'query': query, 'variables': variables})
+        response = json.loads(response.text)['data']['Page']['media']
+        # [data][page][media]
 
-    url = 'https://graphql.anilist.co'
-    response = requests.post(url, json={'query': query, 'variables': variables})
+        for i in response:
+            if i['streamingEpisodes']:
+                sitelist = []
+                site_results = []
+                if i['coverImage']['large']:
+                    cover_img = i['coverImage']['large']
+                else:
+                    cover_img = noimage
+                for j in i['streamingEpisodes']:
+                    if j['site'] not in sitelist:
+                        sitelist.append(j['site'])
+                        site_results.append((j['site'],j['url']))
 
-    return render_template('lab03.html', quote_author=quote_author, quote_text=quote_text)
+                if i['title']['english']:
+                    title = i['title']['english']
+                if i['title']['romaji']:
+                    if title:
+                        title = title + " | " + i['title']['romaji']
+                    else:
+                        title = i['title']['romaji']
+                if i['title']['native']:
+                    if title:
+                        title = title + " | " + i['title']['native']
+                    else:
+                        title = i['title']['native']
+                result_list.append({'Title':title,'Image':cover_img,'Sites':site_results})
+
+    return render_template('lab03.html', result_list=result_list)

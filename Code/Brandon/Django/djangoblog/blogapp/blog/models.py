38d06d3586ab540686d11b6fa76c476e3a6bb3d0
bgonzalez262 <bgonzo262@gmail.com>
2020-03-05 12:04:32 -0800

    Django push

--- Code/Brandon/Django/djangoblog/blogapp/blog/models.py
+++ Code/Brandon/Django/djangoblog/blogapp/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class blogPost (models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=200)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
--- Code/Brandon/Django/djangoblog/blogapp/blog/views.py
+++ Code/Brandon/Django/djangoblog/blogapp/blog/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render, reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+
+from .models import blogPost
+
 
 def index(request):
+
     return render(request,'blog/index.html')
+
+def posts(request):
+    posts = blogPost.objects.order_by('-date')
+    data = []
+    for post in posts:
+        data.append({
+            'id': post.id,
+            'title': post.title,
+            'content': post.content,
+            'date': post.date.strftime('%m/%d/%Y')
+        })
+    return JsonResponse({'blog_posts': data})

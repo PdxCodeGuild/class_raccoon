d5ee9fa5f64d633740ec4054f0b7010fdad3ea12
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2020-04-14 11:27:25 -0700

    now lab 22 b complete :)

--- Code/Alex/python/labs/lab22_atm.py
+++ Code/Alex/python/labs/lab22_atm.py
@@ -1,13 +1,5 @@
 '''
 Lab 22: ATM
-
-Let's represent an ATM with a class containing two attributes: a balance and an interest rate. A newly created account will default to a balance of 0 and an interest rate of 0.1%. Implement the initializer, as well as the following functions:
-
-check_balance() returns the account balance
-deposit(amount) deposits the given amount in the account
-check_withdrawal(amount) returns true if the withdrawn amount won't put the account in the negative
-withdraw(amount) withdraws the amount from the account and returns it
-calc_interest() returns the amount of interest calculated on the account
 '''
 import math
 
--- Code/Alex/python/labs/lab22_atm_v2.py
+++
@@ -1,8 +0,0 @@
-'''
-Lab 22: ATM
-
-Version 2
-
-Have the ATM maintain a list of transactions. Every time the user makes a deposit or withdrawal, add a string to a list saying 'user deposited $15' or 'user withdrew $15'. Add a new function print_transactions() to your class for printing out the list of transactions.
-
-'''

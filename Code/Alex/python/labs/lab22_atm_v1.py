5c2bea8416814eae737c316e08893d11f4ac5a51
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2020-04-14 11:25:59 -0700

    python lab 22 atm complete.

--- Code/Alex/python/labs/lab22_atm_v1.py
+++ Code/Alex/python/labs/lab22_atm_v1.py
@@ -9,93 +9,108 @@ check_withdrawal(amount) returns true if the withdrawn amount won't put the acco
 withdraw(amount) withdraws the amount from the account and returns it
 calc_interest() returns the amount of interest calculated on the account
 '''
+import math
 
 
-import os
-import string
+
+class AtmAccount:
+    def __init__(self, username, password, balance, transactions = []):
+        self.username = username
+        self.password = password
+        self.balance = balance
+        self.transactions = transactions
+
+    def check_credentials(self, username, password):
+        return self.username == username and self.password == password
 
 
 class Atm:
-    def balance(self):
-        return self.current_account.balance
+    def __init__(self, interest=0.001):
+        self.interest = interest
+        self.accounts = [
+            AtmAccount('alex', 'alex', 100),
+        ]
+        self.current_account = None
+
+    def login(self, username, password):
+        for account in self.accounts:
+            if account.check_credentials(username, password):
+                self.current_account = account
+                print('logged in as ' + self.current_account.username)
+                break
+        else:
+            print('could not find a user with that username and password')
+
+    def logout(self):
+        self.current_account = None
 
     def deposit(self, amount):
-        self.current_account.balance += amount #deposits the given amount in the account
+        if self.current_account is None:
+            print('not logged in')
+            return None
+        self.current_account.balance += amount
+        self.__round()
+        self.current_account.transactions.append(f"{self.current_account.username} deposited $%.2f." %amount) #adds entry to transaction history
+        print("$%.2f deposited." %amount)
+        print("Your new balance is $%.2f." %self.current_account.balance)
 
-
-    def check_withdrawal(self, amount) #returns true if the withdrawn amount won't put the account in the negative
-
+    def check_withdrawal(self, amount):
+        return amount <= self.current_account.balance
 
     def withdraw(self, amount):
-        self.current_account.balance -= amount #withdraws the amount from the account and returns it
-
-
-    def calc_interest(self, interest=0.001):
-        self.interest = interest
-
-
-
-
-
-
-#implement teller or self serve
-atm = Atm()
-
-while True:
-    os.system("clear")
-    pin = input("ATM\n\nPIN: ")
-
-    if not pin.isdigit() or len(pin) != 4:
-            print("Invalid PIN")
-            break
-
-    else:
-        account = input("\n\nOptions:\nSavings\nChecking\n\n").lower()
-        if account == "savings":
-
-            duty = input('\n\nOptions:\nCheck balance\nWithdraw\nDeposit\nCancel\n\n').lower()
-
-            if duty == 'balance':
-                print(f"Your current balance in your {account} account is ${}")
-
-            elif duty == 'withdraw':
-                amount = input('Please enter the amount you would like to withdraw\n$')
-
-            elif duty == 'deposit':
-                amount = input('Please enter the amount you would like to deposit\n$')
-
-            elif duty == 'cancel':
-                cancel = input('Cancel transaction? ')
-                if cancel == "yes" or "y":
-                    print("Goodbye!")
-                else:
-                    break
-            else:
-                print('invalid entry')
-                input()
-
-        if account == "checking":
-            duty = input('\n\nOptions:\nCheck balance\nWithdraw\nDeposit\nCancel\n\n').lower()
-
-            if duty == 'balance':
-                print(f"Your current balance in your {account} account is ${}")
-
-            elif duty == 'withdraw':
-                amount = input('Please enter the amount you would like to withdraw\n$')
-
-            elif duty == 'deposit':
-                amount = input('Please enter the amount you would like to deposit\n$')
-
-            elif duty == 'cancel':
-                cancel = input('Cancel transaction? ')
-                if cancel == "yes" or "y":
-                    print("Goodbye!")
-                else:
-                    break
-            else:
-                print('invalid entry')
-                input()
-
+        if self.current_account is None:
+            print('not logged in')
+            return None
+        if self.check_withdrawal(amount): #tests for sufficient funds
+            self.current_account.balance -= amount
+            self.__round()
+            self.current_account.transactions.append("User withdrew $%.2f." %amount) #adds entry to transaction history
+            print("$%.2f withdrawn." %amount)
+            print("Your new balance is $%.2f." %self.current_account.balance)
         else:
-            print('invalid entry')
-            input()
+            print("Insufficient funds! Your current balance is $%.2f." %self.current_account.balance)
+
+    #function for calculating interest rate
+    def calc_interest(self):
+        return round(self.current_account.balance * self.interest, 2)
+
+    #function for displaying list of transactions
+    def display_transactions(self):
+        print(*self.current_account.transactions, sep = '\n')
+
+    #restricts all inputs to two decimal places
+    def __round(self):
+        self.current_account.balance = round(self.current_account.balance, 2)
+
+    def balance(self):
+        if self.current_account is None:
+            print('not logged in')
+            return None
+        return self.current_account.balance
+
+
+atm = Atm()
+userinput = ''
+
+while userinput != 'quit':
+    while True:
+        command = input('what would you like to do? (login, logout, deposit, withdraw, balance, history or quit) ')
+        if command == 'login':
+            username = input('what is your username? ')
+            password = input('what is your password? ')
+            atm.login(username, password)
+        elif command == 'logout':
+            atm.logout()
+        elif command == 'deposit':
+            amount = float(input('what is the amount? $'))
+            atm.deposit(amount)
+        elif command == 'withdraw':
+            amount = float(input('what is the amount? $'))
+            atm.withdraw(amount)
+        elif command == 'balance':
+            print(f'\n\n${atm.balance()}\n\n')
+        elif command == 'history':
+            print("Here's your list of transactions:")
+            atm.display_transactions()
+        else:
+            print('command not recognized')

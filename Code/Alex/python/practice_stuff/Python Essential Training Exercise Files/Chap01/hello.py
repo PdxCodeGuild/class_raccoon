e3d6b880f3d38c38efeee256599f95061bd61f71
zepzappdavis5253 <zepzappdavis5253@gmail.com>
2021-01-10 21:26:30 -0800

    rock paper scissors

---
+++ Code/Alex/python/practice_stuff/Python Essential Training Exercise Files/Chap01/hello.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+import this
+print('\n\n\nHello, World.\n\n\n')
+print('something entirely different\n\n\n')
--- Code/Alex/python/practice_stuff/python Exercise Files/Chap13/hello.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# Copyright 2009-2017 BHG http://bw.org/
-
-print('Hello, World.')
